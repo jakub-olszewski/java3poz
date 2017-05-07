8805dc30bc44eff77844ec9fee58a9beb992e485
jakub-olszewski <j.b.olszewski@gmail.com>
2017-05-07 18:48:30 +0200

    Hello World

---
+++ src/main/java/pl/sdacademy/main/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package pl.sdacademy.main;
+
+/**
+ * @author olszewskij
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
